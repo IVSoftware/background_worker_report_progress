b5a17c7fadedd2f79ed6c1dd6ca92cd18f926eb7
Tom Gregor <tgregor@ivsoftware.com>
2022-06-10 23:40:58 -0600

    Add project files.

---
+++ background_worker_report_progress/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace background_worker_report_progress
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.btnWorker = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Worker Status";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.BackColor = System.Drawing.Color.DimGray;
+            this.labelStatus.ForeColor = System.Drawing.Color.White;
+            this.labelStatus.Location = new System.Drawing.Point(153, 26);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(41, 25);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Idle";
+            // 
+            // btnWorker
+            // 
+            this.btnWorker.Appearance = System.Windows.Forms.Appearance.Button;
+            this.btnWorker.AutoSize = true;
+            this.btnWorker.Location = new System.Drawing.Point(25, 94);
+            this.btnWorker.Name = "btnWorker";
+            this.btnWorker.Size = new System.Drawing.Size(137, 35);
+            this.btnWorker.TabIndex = 2;
+            this.btnWorker.Text = "Toggle Worker";
+            this.btnWorker.UseVisualStyleBackColor = true;
+            this.btnWorker.CheckedChanged += new System.EventHandler(this.btnWorker_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 244);
+            this.Controls.Add(this.btnWorker);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.CheckBox btnWorker;
+    }
+}
+
---
+++ background_worker_report_progress/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace background_worker_report_progress
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Progress += (e) =>
+            {
+                Invoke((MethodInvoker)delegate 
+                {
+                    labelStatus.Text = $"{e.Count} of {e.Total}";
+                });
+            };
+        }
+        public event ProgressEventHandler Progress;
+
+        private void btnWorker_CheckedChanged(object sender, EventArgs e)
+        {
+            if(btnWorker.Checked)
+            {
+                _cts = new CancellationTokenSource();
+                Task.Run(() => 
+                {
+                    var formCount = 10000;
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        if(_cts.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                        // Notify every 100 times.
+                        if((i % 100) == 0)
+                        {
+                            Progress?.Invoke(new ProgressEventArgs(count: i, total: formCount));
+                        }
+                        MockCreateForm();
+                    }
+                    Progress?.Invoke(new ProgressEventArgs(count: formCount, total: formCount));
+                }, _cts.Token);
+            }
+            else
+            {
+                _cts.Cancel();
+                labelStatus.Text = "Idle";
+            }
+        }
+        CancellationTokenSource _cts = null;
+        public void MockCreateForm()
+        {
+            Task.Delay(5).Wait();
+        }
+    }
+
+    public delegate void ProgressEventHandler(ProgressEventArgs e);
+    public class ProgressEventArgs : EventArgs
+    {
+        public ProgressEventArgs(int count, int total)
+        {
+            Count = count;
+            Total = total;
+        }
+        public int Count { get; }
+        public int Total { get; }
+    }
+}
